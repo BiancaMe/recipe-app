2243fef44e2423025f8f64df9adaf3cb942e5651
Chimwemwe Mkandawire <chimzymk@gmail.com>
2024-02-07 11:15:17 +0200

    Create a navigation menu that allows users to open all of the pages created

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  root to: "recipes#index"
+  resources :recipes, only: [:index, :new, :create, :show, :update, :destroy] do
+    resources :recipe_foods
+  end
+ 
+  resources :foods, only: [:index, :new, :create, :destroy]
+  resources :shopping_lists, only: [:index]
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :shopping_lists, only: [:index]
 end
