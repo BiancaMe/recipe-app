6ccb91a2936d9f3bb8a3b6363581bcbc8882e2ba
vikas924 <obroy902@gmail.com>
2024-02-07 22:27:37 +0530

    Add tailwind css and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :inventories, only: [:create, :index, :show, :destroy, :new] do
+    resources :inventory_foods, only: [:new, :create, :destroy]
+  end
 
+  resources :inventory_foods do
+    delete 'remove_food', on: :member
+  end
+
+  resources :foods, only: [:index, :create, :destroy, :new]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root to: "recipes#index"
