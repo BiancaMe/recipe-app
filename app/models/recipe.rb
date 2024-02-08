72277e728a1056fb21f7d1844c6fdfc8d5bd4279
BiancaMe <bianquis0410@gmail.com>
2024-02-07 23:12:18 -0300

    Set up the enviroment for modal and shopping list

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,13 @@
 class Recipe < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :recipe_foods, foreign_key: :recipe_id
+
+  def total_price
+    total = 0
+    recipe_foods.each do |r_food|
+      total += (r_food.quantity * r_food.food.price)
+    end
+    total
+  end
+
 end
