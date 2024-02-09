49d7d3827a43a01012ab7c482330397727fad269
BiancaMe <bianquis0410@gmail.com>
2024-02-09 12:39:44 -0300

    Fiz linters errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
-Recipe.create(name: 'Pizza',preparation_time: 0.5,cooking_time: 30, description: 'Pizza', public: true,user: first_user)
+Recipe.create(name: 'Pizza', preparation_time: 0.5, cooking_time: 30, description: 'Pizza', public: true,
+              user: first_user)
