7cad7ac1496df8e1737d9689a85824a468589eda
BiancaMe <bianquis0410@gmail.com>
2024-02-08 23:22:29 -0300

    Add unit tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Has inventories' do
+    association = described_class.reflect_on_association(:inventories)
+    expect(association.macro).to eq :has_many
+  end
+
+  it 'Has recipes' do
+    association = described_class.reflect_on_association(:recipes)
+    expect(association.macro).to eq :has_many
+  end
 end
