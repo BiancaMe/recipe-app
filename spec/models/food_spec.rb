6a87dd9d263debe78af965f6a9a839051a546815
BiancaMe <bianquis0410@gmail.com>
2024-02-08 23:22:14 -0300

    Add unit tests for food model

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @food = Food.new(name: 'potato', measurement_unit: 'g', price: 2)
+    @food.save
+  end
+
+  it 'Has inventory foods' do
+    association = Food.reflect_on_association(:inventory_foods)
+    expect(association.macro).to eq(:has_many)
+  end
+
+  it 'Has recipe foods' do
+    association = Food.reflect_on_association(:recipe_foods)
+    expect(association.macro).to eq(:has_many)
+  end
 end
