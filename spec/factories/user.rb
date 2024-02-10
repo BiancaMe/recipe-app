e4b4c71d86bd6f0616d9c143db309838b7d0a979
vikas924 <obroy902@gmail.com>
2024-02-10 14:31:26 +0530

    update

--- spec/factories/user.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    name { 'John Doe' }
-    email { 'john@example.com' }
-    password { 'password123' }
-    password_confirmation { 'password123' }
-  end
-end
