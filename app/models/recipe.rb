8c259005f921da6fbfab87dd7a1eb5b244c3edc8
vikas924 <obroy902@gmail.com>
2024-02-10 23:30:18 +0530

    Add fix

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user, class_name: 'User'
-  has_many :recipe_foods, foreign_key: :recipe_id
+  has_many :recipe_foods, dependent: :destroy
+  has_many :foods, through: :recipe_foods
 
   validates :name, presence: true
 
